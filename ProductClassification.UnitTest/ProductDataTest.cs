2b52e015e01a18a5d02d37960de485384861b5bc
hmm0209 <hmm0209@naver.com>
2020-07-23 16:14:14 +0900

    리드미 추가

--- ProductClassification.UnitTest/ProductDataTest.cs
+++ ProductClassification.UnitTest/ProductDataTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProductClassification.Data;
 
+
 namespace ProductClassification.UnitTest
 {
     [TestClass]
